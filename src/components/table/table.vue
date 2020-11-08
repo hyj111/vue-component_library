e0de749cb37c3988d31f944df4ceaa2ca620da6a
hyj111 <1069234622@qq.com>
2020-11-08 21:22:53 +0800

    表格按钮逻辑完善，加入插槽

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,58 +1,95 @@
 <template>
   <div>
-     <el-table :data="tableData" style="width: 100%" border>
+    <el-table :data="tableData" style="width: 100%" border>
       <template v-for="item in tableConfig">
-         <el-table-column
-          v-if="item.type == 'operation'"
+        <!-- 回调，数据需要转换后显示 -->
+        <el-table-column
+          :prop="item.prop"
           :label="item.label"
           :key="item.prop"
+          :width="item.width"
+          v-if="item.type === 'function'"
         >
-        <template  slot-scope="scope">
-          <el-button 
-          v-for="btn in item.buttonGroup"
-          :key="btn.id" 
-          :type="btn.type"
-          @click="btn.handler && btn.handler(scope.row)" 
-          size="small">
-          {{btn.label}}
-          </el-button>
-        </template>
-        
+          <template slot-scope="scope">
+            <span
+              v-html="item.callback && item.callback(scope.row, item.prop)"
+            ></span>
+          </template>
         </el-table-column>
-        
+        <!-- 图片 -->
         <el-table-column
-          v-else
+          :prop="item.prop"
+          :label="item.label"
+          :key="item.prop"
+          :width="item.width"
+          v-else-if="item.type === 'image'"
+        >
+          <template slot-scope="scope">
+            <img
+              :src="'https://forumimg01.jiaoliuqu.com/' + scope.row[item.prop]"
+              :width="item.imgWidth || 50"
+            />
+          </template>
+        </el-table-column>
+        <!-- 操作 -->
+        <el-table-column
+          v-else-if="item.type == 'operation'"
+          :label="item.label"
+          :key="item.prop"
+          align="center"
+        >
+          <template slot-scope="scope">
+            <template v-for="(operation,index) in item.buttonGroup">
+              <el-link
+                v-if="operation.element === 'link'"
+                v-show="!operation.callback || (operation.callback && operation.callback(scope.row, item.prop))"
+                :key="index"
+                :underline="false"
+                :type="operation.type"
+                @click="operation.handler && operation.handler(scope.row)"
+              >
+                {{ operation.label }}
+              </el-link>
+              <!-- 当operation.element==divider且operation.callback为空时或者operation.callback的返回值是true时候显示， -->
+                <el-divider 
+                v-else-if="operation.element=='divider'&&(!operation.callback || (operation.callback && operation.callback(scope.row, item.prop)))"      
+                direction="vertical" 
+                :key="index"  
+               ></el-divider>
+                <slot v-else-if="operation.element === 'slot'" :name="operation.slotName" :data="scope.row" ></slot>
+            </template>
+          </template>
+        </el-table-column>
+        <!-- 纯文本 -->
+        <el-table-column
+          v-else-if="item.type == 'text'"
           :prop="item.prop"
           :label="item.label"
           :key="item.prop"
         ></el-table-column>
       </template>
-       
-  </el-table>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {};
   },
   props: {
-    tableConfig:{
+    tableConfig: {
       type: Array,
-      default: ()=>[]
+      default: () => [],
     },
-    tableData:{
+    tableData: {
       type: Array,
-      default: ()=>[]
-    }
+      default: () => [],
+    },
   },
-  mounted() {
-    
-  }
-}
+  mounted() {},
+};
 </script>
 
 <style scoped>
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,179 +1,260 @@
 <template>
   <div>
-    <!-- <Table :tableConfig="tableConfig" :tableData="tableData" /> -->
-    <el-dialog
-    title="提示"
-    :visible.sync="dialogVisible"
-    >
-    </el-dialog>
-    <Form :formConfig="formConfig" :formData="formData" :formHandler="formHandler" ref="vuForm"/>
+    <Table :tableConfig="tableConfig" :tableData="tableData">
+      <template v-slot:delete="slotData">
+        <el-popconfirm title="确定删除该数据吗？" @confirm="deleteData(slotData)">
+          <el-link slot="reference" type="danger" :underline="false">删除</el-link>
+        </el-popconfirm>
+      </template>
+    </Table>
+    <el-dialog title="提示" :visible.sync="dialogVisible"> </el-dialog>
+    <!-- <Form :formConfig="formConfig" :formData="formData" :formHandler="formHandler" ref="vuForm"/> -->
+           <el-divider 
+                v-show="false"          
+                direction="vertical" 
+               ></el-divider>
   </div>
 </template>
 
 <script>
 import Table from "@/components/table/table";
-import Form from "@/components/form/form"
+import Form from "@/components/form/form";
 export default {
   components: {
     Table,
-    Form
+    Form,
   },
   data() {
     return {
       // 配置表格
       tableConfig: [
-         {
-          label: 'ID',
-          prop: 'id'
+        {
+          label: "ID",
+          prop: "id",
+          type: "text",
         },
         {
-          label: 'cloned',
-          prop: 'cloned'
+          label: "cloned",
+          prop: "cloned",
+          type: "text",
         },
         {
-          label: '平台',
-          prop: 'platformId'
+          label: "平台",
+          prop: "platformId",
         },
         {
-          label: '渠道',
-          prop: 'channel'
+          label: "渠道",
+          prop: "channel",
+          type: "text",
         },
         {
-          label: '版本',
-          prop: 'appVersion'
+          label: "版本",
+          prop: "appVersion",
+          type: "text",
         },
         {
-          label: '环境',
-          prop: 'env'
+          label: "环境",
+          prop: "env",
+          type: "text",
         },
         {
-          label: '当前配置版本',
-          prop: 'appcode'
+          label: "当前配置版本",
+          prop: "appcode",
+          type: "text",
         },
         {
-          label: '创建时间',
-          prop: 'createTime'
+          label: "创建时间",
+          prop: "createTime",
+          type: "text",
         },
         {
-          label: '更新时间',
-          prop: 'updateTime'
+          label: "更新时间",
+          prop: "updateTime",
+          type: "text",
         },
         {
           type: "operation",
           label: "操作",
           buttonGroup: [
-            {
-              type: "danger",
-              label: "编辑",
-              handler:(data) =>{console.log(data);}
+            { 
+              element: "link",
+              type: "primary",
+              label: "通过",
+              handler: (data) => {
+                console.log(data);
+              },
+              callback:function(data){
+                if(data.env=="测试"){
+                  return false
+                }else {
+                  return true
+                }
+              }
             },
-             {
-              type: "danger",
-              label: "编辑",
-              handler:(data) =>{this.dialogVisible = true}
+            {
+              element: 'divider',
+              callback:function(data){
+                if(data.env=="测试"){
+                  return false
+                }else {
+                  return true
+                }
+              }
+            },
+            { 
+              element: "link",
+              type: "primary",
+              label: "拒绝",
+              handler: (data) => {
+                console.log(data);
+              },
+              callback:function(data){
+                if(data.env=="测试"){
+                  return false
+                }else {
+                  return true
+                }
+              }
             },
             {
-              type: "text",
-              label: "111",
-              handler:(data) =>{this.dialogVisible = true}
+              element: 'divider',
+              callback:function(data){
+                if(data.env=="测试"){
+                   console.log(data.env);
+                  return false
+                }else {
+                  return true
+                }
+              }
+            },
+            {
+              element: "slot",
+              slotName: "delete",
             },
           ],
         },
       ],
       // 表格数据
-      // tableData: [
+      tableData: [
+        {
+          appVersion: "980",
+          appcode: "1",
+          channel: "360zhushou",
+          cloned: "0",
+          createTime: "2019-09-16 14:53:44",
+          env: "全环境",
+          id: "175",
+          latestConfigVersion: 119,
+          platformId: "安卓",
+          updateTime: "2020-10-15 14:54:52",
+        },
+        {
+          appVersion: "982",
+          appcode: "2",
+          channel: "360zhushou",
+          cloned: "0",
+          createTime: "2019-09-16 14:53:44",
+          env: "测试",
+          id: "175",
+          latestConfigVersion: 119,
+          platformId: "安卓",
+          updateTime: "2020-10-15 14:54:52",
+        },
+        {
+          appVersion: "980",
+          appcode: "1",
+          channel: "360zhushou",
+          cloned: "0",
+          createTime: "2019-09-16 14:53:44",
+          env: "全环境",
+          id: "175",
+          latestConfigVersion: 119,
+          platformId: "安卓",
+          updateTime: "2020-10-15 14:54:52",
+        },
+      ],
+      dialogVisible: false,
+      // form 表单配置
+      // formConfig: [
       //   {
-      //     appVersion: "980",
-      //     appcode: "1",
-      //     channel: "360zhushou",
-      //     cloned: "0",
-      //     createTime: "2019-09-16 14:53:44",
-      //     env: "全环境",
-      //     id: "175",
-      //     latestConfigVersion: 119,
-      //     platformId: "安卓",
-      //     updateTime: "2020-10-15 14:54:52",
+      //     type: "input",
+      //     label:"停车",
+      //     prop:"name1",
+      //     placeholder:"停车场名称",
+      //   },
+      //   {
+      //     type: "input",
+      //     label:"停车",
+      //     prop:"name2",
+      //     placeholder:"停车场名称",
+      //     requiredMsg:"请输入停车场名称",
+      //     required:true
+      //   },
+      //   {
+      //     type: "input",
+      //     label:"停车",
+      //     prop:"name3",
+      //     placeholder:"停车场名称",
+      //     required:true
+      //   },
+      //   {
+      //     type: "select",
+      //     label: "类型",
+      //     prop: "type",
+      //     required:true,
+      //      options: [
+      //       {
+      //         value: '1',
+      //         label: '1'
+      //       },
+      //       {
+      //         value: '8',
+      //         label: '8'
+      //       },
+      //       {
+      //         value: '12',
+      //         label: '12'
+      //       }
+      //     ]
       //   },
       // ],
-      dialogVisible:false,
-      // form 表单配置
-      formConfig: [
-        {
-          type: "input",
-          label:"停车",
-          prop:"name1",
-          placeholder:"停车场名称",
-        },
-        {
-          type: "input",
-          label:"停车",
-          prop:"name2",
-          placeholder:"停车场名称",
-          requiredMsg:"请输入停车场名称",
-          required:true
-        },
-        {
-          type: "input",
-          label:"停车",
-          prop:"name3",
-          placeholder:"停车场名称",
-          required:true
-        },
-        {
-          type: "select",
-          label: "类型",
-          prop: "type",
-          required:true,
-           options: [
-            {
-              value: '1',
-              label: '1'
-            },
-            {
-              value: '8',
-              label: '8'
-            },
-            {
-              value: '12',
-              label: '12'
-            }
-          ]
-        },
-      ],
       // form表单按钮的配置
-      formHandler: [
-         {
-              type: "danger",
-              label: "编辑",
-              handler:(data) =>{console.log(data);}
-            },
-             {
-              type: "success",
-              label: "提交",
-              handler:()=>this.onSubmit()
-            },
-            {
-              type: "text",
-              label: "111",
-              handler:(data) =>{this.dialogVisible = true}
-            },
-      ],
+      // formHandler: [
+      //    {
+      //         type: "danger",
+      //         label: "编辑",
+      //         handler:(data) =>{console.log(data);}
+      //       },
+      //        {
+      //         type: "success",
+      //         label: "提交",
+      //         handler:()=>this.onSubmit()
+      //       },
+      //       {
+      //         type: "text",
+      //         label: "111",
+      //         handler:(data) =>{this.dialogVisible = true}
+      //       },
+      // ],
       // form表单数据
-      formData:[],
+      formData: [],
     };
   },
-  methods:{
-    onSubmit () {
-        this.$refs.vuForm.$refs.form.validate(valid => {
+  methods: {
+    deleteData(data){
+      console.log(data);
+    },
+    onSubmit() {
+      this.$refs.vuForm.$refs.form.validate((valid) => {
         if (valid) {
-          alert("submit!!")
+          alert("submit!!");
         } else {
           console.log("error submit!!");
           return false;
         }
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
